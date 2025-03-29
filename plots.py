ec422d4f0c6b3bec760ab6f67dda52a4a8fa0b94
veneno <vitor.simbalista@unifesp.br>
2025-03-29 18:54:00 -0300

    Train val curve plots

--- plots.py
+++ plots.py
@@ -12,33 +12,16 @@ results_path = "results/Effisegnet results.xlsx"
 results = pd.read_excel(results_path)
 
 results = results.rename(columns={'test_loss': "Loss", 'test_accuracy': "Accuracy",
-                                  'test_dice': "Dice", 'test_iou': "IoU",
+                                  'test_dice': "Dice_ERRADO", 'test_iou': "IoU",
                                   'test_recall': "Recall", 'test_precision': "Precision",
-                                  'test_f1':"F1-score"})
+                                  'test_f1':"Dice"})
 
 df = pd.melt(results, id_vars=["N fold"], var_name="Metric Name", value_name="Metric Value")
-
-#%% Violin Plot Example
-
-# df = px.data.tips()
-# fig = px.violin(df, y="tip", x="smoker", color="sex", box=True, points="all",
-#           hover_data=df.columns)
-# fig.show()
-# fig.write_image("fig1.svg")
-
-#%% Violin Plot #1: All
-
-fig = px.violin(df, y="Metric Value", x="Metric Name", color="Metric Name", box=True, points="all",
-          hover_data=df.columns, title="Resultados Effisegnet K-fold k=10")
-
-fig.show()
-fig.write_image("results/fig1.svg", width=1000, height=500)
-fig.write_image("results/fig1.png", width=1000, height=500)
-
+ 
 #%% Violin Plot #2: Not Loss
 
 df = df[df["Metric Name"]!="Loss"]
-df = df[df["Metric Name"]!="F1-score"]
+df = df[df["Metric Name"]!="Dice_ERRADO"]
 fig = px.violin(df, y="Metric Value", x="Metric Name", color="Metric Name",
                 box=True, points="all", hover_data=df.columns, #title="Resultados Effisegnet K-fold k=10"
 )
@@ -54,5 +37,5 @@ fig.update_layout(
 )
 
 fig.show()
-# fig.write_image("results/fig2.svg", width=1000, height=500)
-fig.write_image("results/fig2.png", width=1200, height=600)
+fig.write_image("results/fig3.svg", width=1000, height=500)
+fig.write_image("results/fig3.png", width=1200, height=600)
